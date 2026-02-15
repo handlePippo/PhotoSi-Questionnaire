ae0c7e0a26858d86a2baad5c7a3dd116906068dc
filippopalliani <filippopallianimail@gmail.com>
2026-02-15 23:35:13 +0100

    renamed project

--- src/lib/PhotoSi.Infrastructure.Application/obj/Debug/net8.0/PhotoSi.Infrastructure.Application.AssemblyInfo.cs
+++ src/lib/PhotoSi.Infrastructure.Application/obj/Debug/net8.0/PhotoSi.Infrastructure.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoSi.Infrastructure.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+291dc706339733feec299d4c3cd91c94d4d99dbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoSi.Infrastructure.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoSi.Infrastructure.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/lib/PhotoSi.Questionnaire.Api/obj/Debug/net8.0/PhotoSi.Questionnaire.Api.AssemblyInfo.cs
+++ src/lib/PhotoSi.Questionnaire.Api/obj/Debug/net8.0/PhotoSi.Questionnaire.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoSi.Questionnaire.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+291dc706339733feec299d4c3cd91c94d4d99dbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoSi.Questionnaire.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoSi.Questionnaire.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
